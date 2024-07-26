2d2ba28d122a744a4bfab46636083381259d3e8d
KuromiSoma <tamagoyaki.zyukuzyuku@gmail.com>
2024-07-26 18:36:58 +0900

    合ってるのかな

---
+++ src/main/java/org/drug/store/hplist/HpList.java
@@ -0,0 +1,13 @@
+package org.drug.store.hplist;
+
+import lombok.Data;
+
+@Data
+public class HpList {
+
+	private int hpNo;
+	private String hpName;
+	private String hpTel;
+	private String hpCd;
+
+}
---
+++ src/main/java/org/drug/store/hplist/HpListForm.java
@@ -0,0 +1,13 @@
+package org.drug.store.hplist;
+
+import lombok.Data;
+
+@Data
+public class HpListForm {
+
+	private int hpNo;
+	private String hpName;
+	private String hpTel;
+	private String hpCd;
+
+}
